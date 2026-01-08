746b453cdbe7a5e7e7afec183fe8fe822e772b46
Bourzak Douaa <fatima.zohra.doua.bourzak@ensia.edu.dz>
2026-01-08 17:05:31 +0100

    Update treatments_event.dart

--- lib/features/dashboard/bloc/treatments_event.dart
+++ lib/features/dashboard/bloc/treatments_event.dart
@@ -7,6 +7,18 @@ abstract class TreatmentsEvent extends Equatable {
   List<Object?> get props => [];
 }
 
-class LoadTreatmentsChart extends TreatmentsEvent {}
+class LoadTreatmentsChart extends TreatmentsEvent {
+  final int year;
+  const LoadTreatmentsChart({required this.year});
 
-class RefreshTreatmentsChart extends TreatmentsEvent {}
+  @override
+  List<Object?> get props => [year];
+}
+
+class RefreshTreatmentsChart extends TreatmentsEvent {
+  final int year;
+  const RefreshTreatmentsChart({required this.year});
+
+  @override
+  List<Object?> get props => [year];
+}
